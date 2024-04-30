e3bc913aa88ee6ae6789d90ffe041283f0c36905
Santosh Narayanan <santosh33@gmail.com>
2024-04-30 17:18:15 +0200

    template contents and static files

--- my_site/settings.py
+++ my_site/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / "blog/templates/blog/static"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
